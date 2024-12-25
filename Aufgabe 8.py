95125bda601e019cdc19d8d325230321e1dad0b1
Eric345343434 <je3301@mci4me.at>
2024-12-25 17:29:44 +0100

    Aufgabe 8 updated

--- Aufgabe 8.py
+++ Aufgabe 8.py
@@ -49,13 +49,7 @@ def process_emg(data, fs):
     })
     return result
 
-# MVC-Wert (Maximum Voluntary Contraction)
-MVC = 18.157144482993942  # mV
-sampling_frequency = 411.76  # Hz
-weights, mvc, fatigue = lf3.import_data(';')
-mvc_processed = process_emg(mvc, sampling_frequency)
-weights_processed = process_emg(weights, sampling_frequency)
-fatigue_processed = process_emg(fatigue, sampling_frequency)
+
 
 
 def process_emg(data, fs):
@@ -76,16 +70,81 @@ def process_emg(data, fs):
     })
     return result
 
-#wheights plotting
+
+# MVC-Wert (Maximum Voluntary Contraction)
+MVC = 18.157144482993942  # mV
+sampling_frequency = 411.76  # Hz
+weights, mvc, fatigue = lf3.import_data(';')
+mvc_processed = process_emg(mvc, sampling_frequency)
+weights_processed = process_emg(weights, sampling_frequency)
+fatigue_processed = process_emg(fatigue, sampling_frequency)
+
+# Normalize the EMG envelope to MVC
+weights_processed['emg_envelope_normalized'] = weights_processed['emg_envelope'] / MVC * 100
+
+# Weights plotting
 plt.figure()
 
 plt.subplot(2, 1, 1)
-
-plt.plot(weights_processed['t'], weights_processed['emg_filtered'], label='Filtered EMG')
-
+plt.plot(weights_processed['t'], weights_processed['emg_envelope_normalized'], label='Filtered EMG (Normalized)')
 plt.legend()
 plt.title('Weights Experiment')
 plt.xlabel('Time [s]')
-plt.ylabel('EMG [mV]')
+plt.ylabel('EMG [% MVC]')
 plt.grid()
+plt.show()
+
+import Lab3Functions as lf3
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# MVC-Wert (Maximum Voluntary Contraction)
+MVC = 18.157144482993942  # mV
+sampling_frequency = 411.76  # Hz
+
+# Gewichtsdaten einlesen
+weights1 = pd.read_csv('Weight1.txt', delimiter=';')  # 2.5 kg
+weights2 = pd.read_csv('Weight2.txt', delimiter=';')  # 5 kg
+weights3 = pd.read_csv('Weight3.txt', delimiter=';')  # 10 kg
+
+# Indexlängen der Gewichtsdaten finden
+weights1_length = len(weights1)
+weights2_length = len(weights2)
+weights3_length = len(weights3)
+
+weights1_processed = weights_processed[:weights1_length]
+weights2_processed = weights_processed[ weights1_length:weights1_length+weights2_length]
+weights3_processed = weights_processed[weights1_length+weights2_length:weights1_length+weights2_length+weights3_length]
+# Zeit von weights1-3 jeweils von 0 starten lassen
+weights1_processed['t'] = weights1_processed['t'] - weights1_processed['t'].iloc[0]
+weights2_processed['t'] = weights2_processed['t'] - weights2_processed['t'].iloc[0]
+weights3_processed['t'] = weights3_processed['t'] - weights3_processed['t'].iloc[0]
+
+# Plotten von weights1
+plt.figure()
+plt.subplot(3, 1, 1)
+plt.plot(weights1_processed['t'], weights1_processed['emg_envelope_normalized'], label='2.5 kg')
+plt.title('Weights Experiment - 2.5 kg')
+plt.xlabel('Zeit in s')
+plt.ylabel('Anteil von MVC in %')
+plt.grid()
+
+# Plotten von weights2
+plt.subplot(3, 1, 2)
+plt.plot(weights2_processed['t'], weights2_processed['emg_envelope_normalized'], label='5 kg')
+plt.title('Weights Experiment - 5 kg')
+plt.xlabel('Zeit in s')
+plt.ylabel('Anteil von MVC in %')
+plt.grid()
+
+# Plotten von weights3
+plt.subplot(3, 1, 3)
+plt.plot(weights3_processed['t'], weights3_processed['emg_envelope_normalized'], label='10 kg')
+plt.title('Weights Experiment - 10 kg')
+plt.xlabel('Zeit in s')
+plt.ylabel('Anteil von MVC in %')
+plt.grid()
+
+plt.tight_layout()
 plt.show()
